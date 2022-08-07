b09a7507e1f7c78f8a2c705d658c5da9f65d3459
Gautam Jha <gautamjha3112002@gmail.com>
2022-08-07 21:50:37 +0530

    created a simple static spring boot app with dockerfile and kubernetes deployment and service

---
+++ src/main/java/devops/k8sapp/controller/K8sController.java
@@ -0,0 +1,15 @@
+package devops.k8sapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class K8sController {
+
+    @GetMapping("/greet")
+    public String greetings(){
+        return "Welcome to Kubernetes project with me (Gautam)";
+    }
+}
